4e7857373a225354277db267bc6e4b3027a6f625
SunnyDay <1246771571@qq.com>
2022-08-31 22:17:12 +0800

    异步Dao 与携程

--- app/src/main/java/com/example/noteroom/eg/dao/UserDao.kt
+++ app/src/main/java/com/example/noteroom/eg/dao/UserDao.kt
@@ -2,46 +2,22 @@ package com.example.noteroom.eg.dao
 
 import androidx.room.*
 import com.example.noteroom.eg.entity.User
-import kotlinx.coroutines.flow.Flow
 
 /**
  * Create by SunnyDay /08/24 21:49:56
  */
 @Dao
 interface UserDao {
+
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insert(user: User): Long
+    suspend fun insertUser(user: User)
 
     @Update
-    fun updateUser(newUser: User)
+    suspend fun updateUser(user: User)
+
+    @Delete
+    suspend fun deleteUser(user: User)
 
     @Query("select * from User")
-    fun queryAllUsers(): List<User>
-
-    @Delete
-    fun deleteUser(user: User): Int
-
-    @Delete
-    fun deleteUsers(userList: List<User>)
-
-    /**
-     * 根据指定的名字删除某一行信息。
-     * */
-    @Query("delete from User where name = :targetName ")
-    fun deleteUserByName(targetName: String): Int
-
-    /**
-     * 注意：
-     * age为数据库中列字段名，也即User表中定义的字段
-     * :age代表取值，取方法内传递来的参数。
-     * */
-    @Query("select * from User where age > :age ")
-    fun queryUserOlderThan(age: Int): List<User>
-
-    // for test
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertUsers(vararg users: User)
-
-    @Query("SELECT * FROM user WHERE id = :id")
-    fun loadUserById(id: Int): Flow<User>
+    suspend fun queryUser(): List<User>
 }
--- app/src/main/java/com/example/noteroom/ui/EgActivity.kt
+++ app/src/main/java/com/example/noteroom/ui/EgActivity.kt
@@ -11,74 +11,69 @@ import com.example.noteroom.eg.db.AppDataBase
 import com.example.noteroom.eg.entity.User
 import com.example.noteroom.utils.Logger
 import kotlinx.android.synthetic.main.activity_eg.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.flow.forEach
+import kotlinx.coroutines.launch
 import kotlin.concurrent.thread
 
-class EgActivity : AppCompatActivity() {
+class EgActivity : AppCompatActivity(), CoroutineScope by MainScope() {
     companion object {
         const val TAG = "EgActivity#LOG"
         fun open(context: Context) {
             context.startActivity(Intent(context, EgActivity::class.java))
         }
+
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_eg)
 
-            val userDao = AppDataBase.getDataBase(applicationContext).userDao()
-            val user1 = User("Tom", "boy", 18)
-            val user2 = User("Kate", "girl", 18)
+        val userDao = AppDataBase.getDataBase(applicationContext).userDao()
 
-            adduser1.setOnClickListener {
-                thread {
-                    userDao.insert(user1)
-                    Logger.d(TAG){ "insert user1" }
-                }
+        val user1 = User("Tom", "boy", 18)
+        val user2 = User("Kate", "girl", 18)
+
+        adduser1.setOnClickListener {
+            launch {
+                userDao.insertUser(user1)
             }
+        }
 
-            adduser2.setOnClickListener {
-                thread {
-                    userDao.insert(user2)
-                    Logger.d(TAG){ "insert user2" }
-                }
+        adduser2.setOnClickListener {
+            launch {
+                userDao.insertUser(user2)
             }
-            deleteUser1.setOnClickListener {
-                thread {
-                   // Logger.d(TAG){ "delete:${userDao.deleteUser(user1)}" } // 方法不可用
-
-                   userDao.deleteUser(User("aaa","boy",20).apply {
-                       id = 3
-                   })
-
-                  //   userDao.deleteUserByName("Tom") // 方法可用
-
-                  //  userDao.deleteUsers(arrayListOf(user1,user2)) // 方法不可用
-                }
-
+        }
+        deleteUser1.setOnClickListener {
+            launch {
+                userDao.deleteUser(user1.apply {
+                    id = 1
+                })
             }
-
-            queryAll.setOnClickListener {
-                thread {
-                    val sb = StringBuilder()
-                    userDao.queryAllUsers().forEach {
-                        sb.append("PrimaryKey:${it.id} \n")
-                            .append("userName:${it.name} \n")
-                            .append("userSex:${it.sex} \n")
-                            .append("userAge:${it.age} \n")
-                    }
-                    runOnUiThread {
-                        dbInfo.text = sb.toString()
-                    }
-                }
-            }
+        }
 
         updateUser1.setOnClickListener {
-            thread {
-                val user = user1.copy(name = "jerry", sex = "boy", age = 20).apply {
+            launch {
+                userDao.updateUser(user1.copy("updateName","boy",20).apply {
                     id = 1
+                })
+            }
+        }
+
+        queryAll.setOnClickListener {
+            launch {
+                val sb = StringBuilder()
+                userDao.queryUser().forEach {
+                    sb.append("PrimaryKey:${it.id} \n")
+                        .append("userName:${it.name} \n")
+                        .append("userSex:${it.sex} \n")
+                        .append("userAge:${it.age} \n")
                 }
-                userDao.updateUser(user)
-                Logger.d(TAG){"updateUser id:${user.id}"}
+                runOnUiThread {
+                    dbInfo.text = sb.toString()
+                }
             }
         }
 
